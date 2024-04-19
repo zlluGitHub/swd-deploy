a78d97793f8790b1b972fa2aabcd75773efe5c3e
zll <18230086651@163.com>
2024-04-19 18:09:35 +0800

    # update to dir

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ const chalk = require('chalk') //命令行颜色
 const ora = require('ora') // 加载流程动画
 const spinner_style = require('./src/spinner_style') //加载动画样式
 const shell = require('shelljs') // 执行shell命令
-const node_ssh = require('node-ssh') // ssh连接服务器
+const Client = require('ssh2-sftp-client') // ssh连接服务器
 const inquirer = require('inquirer') //命令行交互
 const zipFile = require('compressing') // 压缩zip
 // const fs = require('fs') // nodejs内置文件模块
 const path = require('path') // nodejs内置路径模块
-const SSH = new node_ssh()
+const SSH = new Client()
 let CONFIG = {};
 try {
   CONFIG = require(`${pathHierarchy}deploy.config.js`) // 项目配置
@@ -35,7 +35,7 @@ let distDir = "", distZipPath = "";
 
 //项目打包代码 npm run build 
 const buildDist = async () => {
-  const loading = ora(defaultLog('项目开始打包')).start()
+  const loading = ora(defaultLog('开始打包项目')).start()
   loading.spinner = spinner_style[config.loadingStyle || 'arrow4']
   shell.cd(path.resolve(__dirname, pathHierarchy))
   const res = await shell.exec(config.buildShell || 'npm run build') //执行shell 打包命令
@@ -73,96 +73,59 @@ const connectSSH = async () => {
     host: config.sshIp,
     username: config.sshUserName,
     port: config.port || 22,
-    [type]: data
+    [type]: data,
+    tryKeyboard: true,
   }
   if (config.readyTimeout) {
     opt.readyTimeout = config.readyTimeout
   }
+
   try {
-    await SSH.connect(opt)
+    // await SSH.connect(opt)
+    await SSH.connect({
+      // host: '127.0.0.1',
+      // port: '8080',
+      // username: 'username',
+      // password: '******'
+      ...opt
+    })
     successLog('服务器连接成功!')
   } catch (error) {
-    errorLog(error)
     errorLog('SSH连接失败! (可能原因: 1:密码不对, 2:privateKey 本机私钥地址不对, 3:服务器未配置本机公钥')
     process.exit() //退出流程
   }
+
   loading.stop()
 }
-
-//线上执行命令
-/**
- * @param {String} command 命令操作 如 ls
- */
-const runCommand = async (command) => {
-  const result = await SSH.exec(command, [], {
-    cwd: config.wwwPath
-  }).catch(err => {
-    errorLog(err)
-    process.exit() //退出流程
-  })
-  // defaultLog(result)
-}
-
-// //清空线上目标目录里的旧文件
-// const clearOldFile = async () => {
-//   const commands = ['ls', `rm -rf ${config.wwwPath}`]
-//   await Promise.all(commands.map(async (it) => {
-//     return await runCommand(it)
-//   }))
-// }
-
-// // 创建线上部署目录
-// const mkdirDir = async () => {
-//   console.log('asas');
-//   await SSH.exec(`mkdir test1`, [], {
-//     cwd: config.wwwPath
-//   }).then(err => {
-//     console.log('创建成功');
-//   }).catch(err => {
-//     errorLog(err)
-//     // process.exit() //退出流程
-//   })
-//   // defaultLog(result)
-// }
-
-//清空线上目标目录里的旧文件
-const clearOldFile = async () => {
-  await SSH.exec(`rm -rf ${config.wwwPath}`, [], {
-    cwd: config.wwwPath
-  }).catch(async err => {
-    errorLog(err)
-    // await mkdirDir()
-    process.exit() //退出流程
-  })
-}
-
-
 
 //传送zip文件到服务器
 const uploadZipBySSH = async () => {
   //连接ssh
   await connectSSH()
   //线上目标文件清空
-  await clearOldFile()
-  const loading = ora(defaultLog(`正在上传 ${config.distFolder}.zip 文件`)).start()
+  // await clearOldFile()
+  const loading = ora(defaultLog(`正在上传 ${config.distFolder} 文件夹`)).start()
   loading.spinner = spinner_style[config.loadingStyle || 'arrow4']
   try {
-    await SSH.putFiles([{
-      local: distZipPath,
-      remote: config.wwwPath + `/${config.distFolder}.zip`
-    }]).then(() => {
-      successLog(`${config.distFolder}.zip 上传成功!`)
-    }) //local 本地  remote 服务器 
 
-    loading.text = '正在解压文件...'
-    await runCommand(`unzip ./${config.distFolder}.zip`) //解压
-    await runCommand(`rm -rf ${config.wwwPath}/${config.distFolder}.zip`) //解压完删除线上压缩包
-    //将目标目录的dist里面文件移出到目标文件  
-    //举个例子 假如我们部署在 /test/html 这个目录下 只有一个网站, 那么上传解压后的文件在 /test/html/dist 里
-    //需要将 dist 目录下的文件 移出到 /test/html   多网站情况, 如 /test/html/h5  或者 /test/html/admin 都和上面同样道理
-    await runCommand(`mv -f ${config.wwwPath}/${config.distFolder}/*  ${config.wwwPath}`)
-    await runCommand(`rm -rf ${config.wwwPath}/${config.distFolder}`) //移出后删除 dist 文件夹
-    SSH.dispose() //断开连接
+    await SSH.uploadDir(config.distFolder, config.wwwPath)
+
+    successLog(`文件夹 ${config.distFolder} 上传成功!`)
+
+    // await SSH.fastPut(distZipPath, config.wwwPath + `/${config.distFolder}.zip`).then(() => {
+    //   successLog(`${config.distFolder}.zip 上传成功!`)
+    // })
+
+    // loading.text = '正在解压文件...'
+    // await SSH.exec(`unzip ./${config.distFolder}.zip`) //解压
+    // await SSH.exec(`rm -rf ${config.wwwPath}/${config.distFolder}.zip`) //解压完删除线上压缩包
+    // //将目标目录的dist里面文件移出到目标文件  
+    // //举个例子 假如我们部署在 /test/html 这个目录下 只有一个网站, 那么上传解压后的文件在 /test/html/dist 里
+    // //需要将 dist 目录下的文件 移出到 /test/html   多网站情况, 如 /test/html/h5  或者 /test/html/admin 都和上面同样道理
+    // await SSH.exec(`mv -f ${config.wwwPath}/${config.distFolder}/*  ${config.wwwPath}`)
+    // await SSH.exec(`rm -rf ${config.wwwPath}/${config.distFolder}`) //移出后删除 dist 文件夹
+
+    SSH.end(); //断开连接
   } catch (error) {
     errorLog(error)
     errorLog('上传失败!')
